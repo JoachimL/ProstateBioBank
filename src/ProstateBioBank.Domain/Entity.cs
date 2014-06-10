dc3675f59defe33a7c0e5c2788789286cbaa7a52
joachim.lovf <joachim.lovf@gmail.com>
2014-06-10 23:19:15 +0200

    files missing from previous commit

---
+++ src/ProstateBioBank.Domain/Entity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ProstateBioBank.Domain
+{
+    public class Entity
+    {
+        public Entity()
+        {
+            Added = DateTime.Now;
+        }
+
+        [Required]
+        public DateTime Added { get; set; }
+    }
+}
---
+++ src/ProstateBioBank.WebSite/Models/PatientIndexViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ProstateBioBank.ServiceModels;
+
+namespace ProstateBioBank.Models
+{
+    public class PatientIndexViewModel
+    {
+        public IEnumerable<Patient> Patients { get; set; }
+    }
+}
